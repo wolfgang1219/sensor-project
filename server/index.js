86f23fa74daa76d1947fe15a58ab333ef740a39d
wolfgang1219 <1354246500@qq.com>
2018-11-18 20:39:38 +0800

    repress init

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.get("/temperature", (req, res) => {
+	res.send("24°C");
+});
+app.get("/humidity", (req, res) => {
+	res.send("48%");
+});
+app.listen(3000, () => {
+	console.log("Srever listening on port 3000");
+});
