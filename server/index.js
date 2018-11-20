f8bde250ed01b9993e676bedde253052711ffdd4
wolfgang1219 <1354246500@qq.com>
2018-11-20 15:46:21 +0800

    index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = express("path");
+app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/temperature", (req, res) => {
 	res.send("24°C");
 });
